68192ab2782d761186084bab8ac989485701b997
Oliver <129097817+gingeapple182@users.noreply.github.com>
2025-10-27 11:44:06 +0000

    Create basic character
    Created basic character movement with SDL3

--- COMP3016 CW1/COMP3016 CW1.cpp
+++ COMP3016 CW1/COMP3016 CW1.cpp
@@ -7,19 +7,16 @@
 
 int main(int argc, char* argv[])
 {
-    //SDL_Init(SDL_INIT_EVERYTHING);
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-	//SDL_Window* window = SDL_CreateWindow("COMP3016 CW1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Window* window = SDL_CreateWindow("COMP3016 CW1", 600, 400, SDL_WINDOW_RESIZABLE);
 	if (!window) {
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
-	//SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 	if (!renderer) {
 		std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
@@ -28,10 +25,58 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-	SDL_Delay(3000);
+	SDL_FRect playerF = { 50.0f, 50.0f, 50.0f, 50.0f };
+	const float playerSpeed = 5.0f;
+	uint64_t lastTicks = SDL_GetTicksNS();
+	int tileSize = 50;
+
+	//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	//SDL_RenderClear(renderer);
+	//SDL_RenderPresent(renderer);
+	//SDL_Delay(3000);
+
+	bool moving = true;
+	SDL_Event event;
+
+	const bool* key = SDL_GetKeyboardState(nullptr);
+	while (moving) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				moving = false;
+			}
+		}
+		
+		SDL_PumpEvents();
+		key = SDL_GetKeyboardState(nullptr);
+		uint64_t now = SDL_GetTicksNS();
+		float dt = (now - lastTicks) / 1000.0f;
+		lastTicks = now;
+		
+		if (key[SDL_SCANCODE_W] || key[SDL_SCANCODE_UP]) {
+			playerF.y -= playerSpeed;
+		}
+		if (key[SDL_SCANCODE_S] || key[SDL_SCANCODE_DOWN]) {
+			playerF.y += playerSpeed;
+		}
+		if (key[SDL_SCANCODE_A] || key[SDL_SCANCODE_LEFT]) {
+			playerF.x -= playerSpeed;
+		}
+		if (key[SDL_SCANCODE_D] || key[SDL_SCANCODE_RIGHT]) {
+			playerF.x += playerSpeed;
+		}
+
+		SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(renderer, &playerF);
+		SDL_RenderPresent(renderer);
+
+		SDL_Delay(16); // ~60 FPS
+	}
+
+	
+
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
